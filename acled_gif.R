d854daa00673e75f269ed885b3fe7335d2bc70a7
seanywng <seanywng@users.noreply.github.com>
2022-03-21 15:52:15 +0800

    pdf version added

--- acled_gif.R
+++ acled_gif.R
@@ -50,10 +50,35 @@ anim_acled <- acled %>%
         plot.caption = element_text(hjust = 0.2)) + 
   transition_manual(month) +
   labs(title = "Conflict events in { current_frame }",
-       subtitle = "Showing 6 most common interaction types\nPeaceful protests have been excluded",
+       subtitle = "Showing the 6 most common interaction types\nPeaceful protests have been excluded",
        caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
        colour = "interaction type")
 
 animate(anim_acled, width = 1748, height = 2480, res = 150, duration = 22)
 
 anim_save("acled4.gif")
+
+# static version for PDF
+
+static <- acled %>% 
+  filter(year == 2021) %>% 
+  filter(inter_type != "sole protester action") %>% 
+  mutate(inter_type = fct_lump(inter_type, 6, other_level = "other")) %>% 
+  select(admin3_pcode, data_id, event_type, longitude, latitude, inter_type, fatalities) %>% 
+  left_join(townships %>% select(admin1_pcode, admin3_pcode), by = "admin3_pcode") %>% 
+  ggplot() +
+  geom_sf(data = pcode1_shape, size = 0.5, colour = "black", alpha = 0) +
+  geom_point(aes(x = longitude, y = latitude, colour = inter_type, size = fatalities)) +
+  scale_colour_manual(values = c("#D95F02", "#7570B3", "#1B9E77", "#E7298A", "#E6AB02", "#00AFBB", "#666666", "#A6761D", "#66A61E")) +
+  theme_void() + 
+  guides(colour = guide_legend(override.aes = list(size = 5), order = 1)) +
+  theme(legend.text = element_text(size = 11),
+        legend.title = element_text(face = "bold"),
+        plot.background = element_rect(fill = "white", colour = NA),
+        plot.caption = element_text(hjust = 0.2)) +
+  labs(title = "Conflict events in 2021",
+       subtitle = "Showing the 6 most common interaction types\n Peaceful protests have been excluded",
+       caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
+       colour = "interaction type")
+
+ggsave(filename = "./static_version_of_gif.png", dpi = 300, height = 16.5, width = 11.7, units = "in")
