8682b9d37877939268b718a7abd2784a3a0619b8
seanywng <seanywng@users.noreply.github.com>
2022-03-28 12:04:02 +0800

    text analytics and general update

--- acled_gif.R
+++ acled_gif.R
@@ -49,8 +49,8 @@ anim_acled <- acled %>%
         legend.title = element_text(face = "bold"),
         plot.caption = element_text(hjust = 0.2)) + 
   transition_manual(month) +
-  labs(title = "Conflict events in { current_frame }",
-       subtitle = "Showing the 6 most common interaction types",
+  labs(title = "Conflict events in { current_frame } by type of interaction",
+       subtitle = "Showing the 6 most common interaction types\n Peaceful protests have been excluded",
        caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
        colour = "interaction type")
 
@@ -76,7 +76,7 @@ static <- acled %>%
         legend.title = element_text(face = "bold"),
         plot.background = element_rect(fill = "white", colour = NA),
         plot.caption = element_text(hjust = 0.2)) +
-  labs(title = "Conflict events in 2021",
+  labs(title = "Conflict events in 2021 by type of interaction",
        subtitle = "Showing the 6 most common interaction types\n Peaceful protests have been excluded",
        caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
        colour = "interaction type")
@@ -105,6 +105,7 @@ event_type <- acled %>%
         plot.caption = element_text(hjust = 0.2)) + 
   transition_manual(month) +
   labs(title = "Types of conflict events in { current_frame }",
+       subtitle = "All events included",
        caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
        colour = "event type")
 
@@ -112,3 +113,25 @@ animate(event_type, width = 1748, height = 2480, res = 150, duration = 22)
 
 anim_save("event_type.gif")
 
+
+# static version of event_types
+acled %>%
+  filter(year == 2021) %>% 
+  select(admin3_pcode, data_id, event_type, longitude, latitude, event_type, month, fatalities) %>% 
+  left_join(townships %>%  select(admin1_pcode, admin3_pcode), by = "admin3_pcode") %>% 
+  ggplot() + 
+  geom_sf(data = pcode1_shape, size = 0.5, colour = "black", alpha = 0) +
+  geom_point(aes(x = longitude, y = latitude, colour = event_type, size = fatalities)) +
+  # scale_colour_manual(values = c("#D95F02", "#7570B3", "#1B9E77", "#E7298A", "#E6AB02", "#00AFBB", "#666666", "#A6761D", "#66A61E")) +
+  theme_void() + 
+  guides(colour = guide_legend(override.aes = list(size = 5), order = 1)) +
+  theme(legend.text = element_text(size = 11), 
+        legend.title = element_text(face = "bold"),
+        plot.caption = element_text(hjust = 0.2),
+        plot.background = element_rect(fill = "white", colour = NA)) +
+  labs(title = "Types of conflict events in 2021",
+       subtitle = "All events included",
+       caption = "Data source: Armed Conflict Location & Event Data Project (ACLED); acleddata.com",
+       colour = "event type")
+
+ggsave(filename = "./static_version_event_type.png", dpi = 300, height = 16.5, width = 11.7, units = "in")
